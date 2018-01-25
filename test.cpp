3c0cff3856161478247921fc08f08effc6d964bc
Carlo Di Ferrante <carlodf@gmail.com>
2018-01-25 20:17:27 +0000

    Removing unused SDL_Renderer and updating comments.

--- test.cpp
+++ test.cpp
@@ -12,26 +12,25 @@ const int SCREEN_HEIGHT = 600;
 
 int main()
 {
+    //SDL2 initialisation.
     raii::Sdl sdl;
-    //The window we'll be rendering to
+
+    //The window we'll be rendering to.
     raii::Window_ptr window(nullptr);
 
-    //The surface contained by the window
+    //The surface contained by the window.
     raii::Surface_ptr screenSurface(nullptr);
 
-    //The surface containing the background (game board)
+    //The surface containing the background (game board).
     raii::Surface_ptr boardSurface(nullptr);
 
-    //The renderer to draw to.
-    raii::Renderer_ptr renderer(nullptr);
-
-    //Initialize SDL
+    //Initialize SDL.
     if(sdl.init(SDL_INIT_VIDEO) < 0)
     {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
-    //Create window
+    //Create window.
     window = raii::Window_ptr(SDL_CreateWindow(
             "SDL Tutorial",
             SDL_WINDOWPOS_UNDEFINED,
@@ -45,17 +44,20 @@ int main()
         return 1;
     }
 
-    //Grab the surface inside the window
+    //Grab the surface inside the window.
     screenSurface = raii::Surface_ptr(SDL_GetWindowSurface(window.get()));
 
+    //Load sprite onto board surface.
     boardSurface = raii::Surface_ptr(SDL_LoadBMP("assets/board.bmp"));
 
+    //Stamp a copy of board surface onto the window surfaec.
     SDL_BlitSurface(boardSurface.get(), NULL, screenSurface.get(), NULL);
 
+    //Update window (believe it switches front and back buffers).
     SDL_UpdateWindowSurface(window.get());
 
 
-    //Wait two seconds
+    //Wait milliseconds
     SDL_Delay(8000);
 
     return 0;
