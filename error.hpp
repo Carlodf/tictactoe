de953b87a4a566cb9ad35d3d29ed8d91297c782c
Carlo Di Ferrante <carlodf@gmail.com>
2018-02-03 13:25:38 +0000

    Started refactoring error
    Added and error structure that holds and error code and an error
    message.

--- error.hpp
+++ error.hpp
@@ -3,6 +3,7 @@
 #include<string>
 #include<cstdio>
 
+//TODO deprecate in favor of Error::log()
 static void error(
     const std::string& message,
     const std::string& error = "")
@@ -14,3 +15,27 @@ static void error(
     }
     std::printf("ERROR: %s [ %s ]\n", message.c_str(), error.c_str());
 }
+
+struct Error
+{
+    Error() = default;
+    Error(
+        std::size_t code,
+        const std::string& message,
+        const char* external_error) :
+        code(code),
+        message(message + std::string(external_error))
+    {}
+
+    Error(std::size_t code, const std::string& message) :
+        code(code),
+        message(message)
+    {}
+
+    void log() const
+    {
+        error(message);
+    }
+    std::size_t code = 0u;
+    std::string message;
+};
